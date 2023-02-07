eeed7b8a6bd6edbfe51a8f5ea1dd82b3eea7003c
JacksenSE <Jacskenbmarshall@gmail.com>
2023-02-06 19:47:28 -0800

    refactored

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -13,6 +14,17 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS
+const bandController = require('./controllers/band_controller')
+app.use('/api/band', bandController)
+
+const eventController = require('./controllers/event_controller')
+app.use('/api/event', eventController)
+
+const stageController = require('./controllers/stage_controller')
+app.use('/api/stage', stageController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
